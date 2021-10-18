9c3b13a684fc774631dd8bce6c67887016307e34
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-17 20:39:50 -0500

    RESTful delete destroy method

---
+++ til20211017.rb
@@ -0,0 +1,6 @@
+#to delete a file using RESTful routues, it is proper to use the .destroy method.
+
+#ie
+file_name.destroy
+
+#this is meant to be built into the controller that is called upon with a DELETE request 
