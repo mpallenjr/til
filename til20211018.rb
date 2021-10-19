82f0acf1331e973d39671ec608da7ff712e08674
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-18 21:36:46 -0500

    rails model inputs that are strings dont need to be labled strings

---
+++ til20211018.rb
@@ -0,0 +1,5 @@
+#when generating a new rails model in terminal, if a given input is a string, there is no need to declare it as a string.
+
+rails generate model Photo location name year:integer camera
+
+###
