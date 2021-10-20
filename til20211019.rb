eaab3f3454e7b012dd6e96b10e59c36c5034c7e1
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-19 22:03:43 -0500

    an if statement is by nature ture or false

---
+++ til20211019.rb
@@ -0,0 +1,16 @@
+#sometimes it pays to know what a given input will return
+def is_discounted?
+  if p > 10
+    return true
+  else
+    return false
+  end
+end
+
+### and ###
+def is_discounted?
+  if p >10
+  end
+end 
+
+#will yield the same output
